ce7c061cb34766b00665cb2bb37831ffba8b6d89
minhnem <dtc2154801030092@ictu.edu.vn>
2025-05-30 19:13:55 +0700

    deploy V5

--- src/constants/appInfo.ts
+++ src/constants/appInfo.ts
@@ -1,7 +1,8 @@
 export const appInfo = {
     title: "HaiDuong",
     description: " ",
-    baseURL: "http://localhost:3001"
+    // baseURL: "http://localhost:3001",
+    baseURL: "https://doan-server-9zrj.onrender.com"
 }
 
 export const localDataNames = {
